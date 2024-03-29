ccdded767c28e9ec8826330e3ac3f9208c212ef9
tindt <tindt@gmail.com>
2024-03-30 01:29:19 +0700

    asdfasdf

--- api/verify.php
+++ api/verify.php
@@ -2,7 +2,7 @@
 $_POST = json_decode(file_get_contents('php://input'), true);
 
 if (isset($_POST) && isset($_POST['g-token'])) {
-    $secretKey = '6LegyagpAAAAAHxCs3YQKZu7FIjzhUJY0dzOhdFG';
+    $secretKey = '6Lf39agpAAAAACF_x_sBXXs43_z9mfi-lgSzD5IH';
     $token = $_POST['g-token'];
     $ip = $_SERVER['REMOTE_ADDR'];
 

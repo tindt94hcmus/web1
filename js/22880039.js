0f19ac59accf147f662e689be18ab51f7627d054
tindt <tindt@gmail.com>
2024-03-30 00:37:00 +0700

    init

---
+++ js/22880039.js
@@ -0,0 +1,23 @@
+const API = 'https://web1-api.vercel.app/api';
+
+async function loadData(request, templateId, viewId) {
+    const response = await fetch(`${API}/${request}`);
+    const data = await response.json();
+
+    var source = document.getElementById(templateId).innerHTML;
+    // console.log(source);
+    var template = Handlebars.compile(source);
+    var context = { data: data };
+    var view = document.getElementById(viewId);
+    view.innerHTML = template(context);
+}
+
+// async function loadData(request, templateId, viewId) {
+//     const response = await fetch(`${API}/${request}`);
+//     const data = await response.json();
+//     var source = document.getElementById({templateId}).innerHTML; !!!
+//     var template = Handlebars.compile(source);
+//     var context = { data: data };
+//     var view = document.getElementById(viewId);
+//     view.innerHTML = template(context);
+// }
